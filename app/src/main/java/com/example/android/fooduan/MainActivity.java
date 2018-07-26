9468c02225b1831de9740cf5e463be1c42704ea3
Samuel Etsiakoh <esgrey094@gmail.com>
2018-07-26 12:55:24 +0000

    first commit

---
+++ app/src/main/java/com/example/android/fooduan/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.fooduan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Sets the screen to fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+}
